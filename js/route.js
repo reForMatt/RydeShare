d4eb78c77f95ce5576b40c9fe519387f7143bf18
Matthew K <reForMatt@users.noreply.github.com>
2016-11-13 08:49:18 -0800

    Add files via upload
    Ready for turnin

--- js/route.js
+++ js/route.js
@@ -1,18 +1,1 @@
-var myApp;
-myApp = angular.module('myApp', ['ngRoute'])
-.config(['$routeProvider', '$locationProvider',
-  function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'route.html',
-      })
-      .when('/Book/:bookId/ch/:chapterId', {
-        templateUrl: 'chapter.html',
-        controller: 'ChapterCtrl',
-        controllerAs: 'chapter'
-      });
 
-    $locationProvider.html5Mode(true);
-}]);
-
-angular.bootstrap(document.getElementById("module2"), ['myApp']);
